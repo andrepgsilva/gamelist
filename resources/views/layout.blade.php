f9e0e9dd944ec43fe5c5de053b79aef8f667263d
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2016-07-06 23:55:17 -0300

    footbar added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,32 @@
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
         <script type="text/javascript" src="{{ elixir('js/jquery.min.js') }}" ></script>
         <script type="text/javascript" src="{{ elixir('js/bootstrap.min.js') }}" ></script>
+        <style>
+            .footer-site .container .col-md-6:first-child {
+                /*margin-top: 30px;*/
+                margin-top: 2%;
+                color: #f4f5f5;
+            }
+
+            .footer-site .container .col-md-6:first-child h1{
+                font-family: PressStart2P;
+                font-size: 20px;
+            }
+
+            .footer-site .container .col-md-6:first-child p{
+                font-family: PressStart2P;
+                font-size: 11px;
+
+            }
+
+            .footer-site .container .col-md-6:last-child {
+                margin-top: 3%;
+            }
+
+            .footer-site .container .col-md-6:last-child .col-md-6 > a{
+                color: #f4f5f5;
+            }
+        </style>
     </head>
     <body>
         <nav class="navbar navbar-inverse">
@@ -36,11 +62,16 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1>Footer header</h1>
-                        <p><p style="color: #F4F5F5 !important;">Todos os direitos reservados a &copy;Andre Pergentino.</p></p>
+                        <h1>Gamelist</h1>
+                        <p>Todos os direitos reservados a &copy; Andre Pergentino</p>
                     </div>
                     <div class="col-md-6">
-                        
+                        <div class="col-md-6 pull-right">
+                             <a href="#"><i class="fa fa-3x fa-fw fa-instagram"></i></a>
+                             <a href="#"><i class="fa fa-3x fa-fw fa-twitter"></i></a>
+                             <a href="#"><i class="fa fa-3x fa-fw fa-facebook"></i></a>
+                             <a href="#"><i class="fa fa-3x fa-fw fa-github"></i></a>
+                         </div>
                     </div>
                 </div>
             </div>
